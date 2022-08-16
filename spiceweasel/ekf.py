29154e08fb46e313f56b8dd25c7a0e25a49509a0
walchko <walchko@users.noreply.github.com>
2022-08-16 09:27:31 -0600

    clean

--- spiceweasel/ekf.py
+++ spiceweasel/ekf.py
@@ -21,6 +21,13 @@ class EKF:
         self.dt = dt
 
     def reset(self, n, m):
+        """
+        Resets the KF to correct dimensions as either an
+        eye or zeros matrix.
+
+        n: state size (Q)
+        m: measurement size (R)
+        """
         self.R = np.eye(m)
         self.Q = np.eye(n)
         self.P = np.eye(n)
@@ -30,31 +37,27 @@ class EKF:
         self.I = np.eye(n)
 
     def predict(self, u):
+        """
+        Predicts the future state (x) and covarience
+        matrix (P). The state predicution uses RK4 to
+        integrate.
+        """
         self.x = self.rk(self.dt, self.x, u)
         F = self.J(self.dt, self.x, u)
         self.P = F @ self.P @ F.T + self.Q
 
     def update(self, z):
+        """
+        Updates the current state estimate using the
+        measurement z.
+        """
         H = self.H
         I = self.I
 
         y = z - H.dot(self.x)
-        # S = H.dot(self.P.dot(H.T)) + self.R
         S = H @ self.P @ H.T + self.R
-        # K = self.P.dot(H.T.dot(np.linalg.inv(S)))
         K = self.P @ H.T @ np.linalg.inv(S)
         self.x = self.x + K.dot(y)
         self.P = (I - K.dot(H)).dot(self.P)
-
-        # q = Quaternion(*self.x[6:])
-        # if abs(1 - q.magnitude) > 1e-6:
-        #     q.normalize
-        #     self.x[6] = q.w
-        #     self.x[7] = q.x
-        #     self.x[8] = q.y
-        #     self.x[9] = q.z
-
-        # self.x = x
-        # self.P = P
 
         return self.x
